32297bc2c306f07d2ff0ae29b726b32404a3b4ea
MCJack123 <jackbruienne@yahoo.com>
2016-07-19 18:02:55 -0400

    Initial Commit

---
+++ Base Tracker UmpireTests/Base_Tracker_UmpireTests.swift
@@ -0,0 +1,36 @@
+//
+//  Base_Tracker_UmpireTests.swift
+//  Base Tracker UmpireTests
+//
+//  Created by Jack Bruienne on 7/19/16.
+//  Copyright © 2016 MCJack123. All rights reserved.
+//
+
+import XCTest
+@testable import Base_Tracker_Umpire
+
+class Base_Tracker_UmpireTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
